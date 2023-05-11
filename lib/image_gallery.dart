a68e1e7d6f5feb29c59cb3ec90014e924e613291
John Bhujel <john.tensa@gmail.com>
2023-05-11 16:34:43 +0545

    feature: added Navigation - gallery to fullscreen

--- lib/image_gallery.dart
+++ lib/image_gallery.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'fullscreen_image.dart';
+
 class GalleryPage extends StatelessWidget {
   final List<String> _imageUrls = [
-    "https://www.example.com/image1.jpg",
-    "https://www.example.com/image2.jpg",
-    "https://www.example.com/image3.jpg",
-    "https://www.example.com/image4.jpg",
-    "https://www.example.com/image5.jpg",
-    "https://www.example.com/image6.jpg",
+    "https://images.unsplash.com/photo-1683464623235-d165004a9e75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://plus.unsplash.com/premium_photo-1680127401733-1e24100f51f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1683463170635-a39ce9b0ecaf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1683662458512-d6692e01ec45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMnx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://plus.unsplash.com/premium_photo-1682542226396-07369c82ad4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1683377117046-99ca0f5a8291?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzN3x8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
   ];
 
   GalleryPage({super.key});
@@ -26,9 +28,19 @@ class GalleryPage extends StatelessWidget {
         ),
         itemCount: _imageUrls.length,
         itemBuilder: (BuildContext context, int index) {
-          return Image.network(
-            _imageUrls[index],
-            fit: BoxFit.cover,
+          return InkWell(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => FullscreenImage(
+                  imageUrl: _imageUrls[index],
+                ),
+              ),
+            ),
+            child: Image.network(
+              _imageUrls[index],
+              fit: BoxFit.cover,
+            ),
           );
         },
       ),

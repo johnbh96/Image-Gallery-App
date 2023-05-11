144943aa04df6f80d58d2791c5a620c798ae122d
John Bhujel <john.tensa@gmail.com>
2023-05-11 16:57:10 +0545

    test: test fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image_gallery/fullscreen_image.dart';
@@ -5,13 +7,13 @@ import 'package:image_gallery/image_gallery.dart';
 
 void main() {
   group('GalleryPage widget', () {
+    setUpAll(() => HttpOverrides.global = null);
     testWidgets('Renders gallery with correct number of images',
         (WidgetTester tester) async {
       await tester.pumpWidget(MaterialApp(home: GalleryPage()));
 
       final gridViewFinder = find.byType(GridView);
       expect(gridViewFinder, findsOneWidget);
-
       final imageFinder = find.byType(Image);
       expect(imageFinder, findsNWidgets(6));
     });
@@ -33,7 +35,8 @@ void main() {
   group('FullscreenImage widget', () {
     testWidgets('Renders fullscreen image with correct image URL',
         (WidgetTester tester) async {
-      const imageUrl = 'https://www.example.com/image.jpg';
+      const imageUrl =
+          'https://images.unsplash.com/photo-1683464623235-d165004a9e75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60';
       await tester.pumpWidget(
           const MaterialApp(home: FullscreenImage(imageUrl: imageUrl)));
 
@@ -49,7 +52,8 @@ void main() {
 
     testWidgets('Returns to previous screen when tapped',
         (WidgetTester tester) async {
-      const imageUrl = 'https://www.example.com/image.jpg';
+      const imageUrl =
+          'https://images.unsplash.com/photo-1683464623235-d165004a9e75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60';
       final navigatorKey = GlobalKey<NavigatorState>();
 
       await tester.pumpWidget(MaterialApp(
